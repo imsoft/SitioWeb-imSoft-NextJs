2833be1515653d81aa6fa84fda89e18fd49f278a
imSoft <weareimsoft@gmail.com>
2023-02-25 19:04:41 -0600

    Se componetizarion las redes sociales y los datos de contacto

--- components/ui/shared/index.ts
+++ components/ui/shared/index.ts
@@ -1,3 +1,4 @@
+export * from './ContactInfo';
 export * from './ConversionBar';
 export * from './Footer';
 export * from './Header';
--- interfaces/footer.ts
+++ interfaces/footer.ts
@@ -5,5 +5,4 @@ export interface IFooter {
   company: ICommon[];
   articles: ICommon[];
   legal: ICommon[];
-  social: ISocial[];
 }
