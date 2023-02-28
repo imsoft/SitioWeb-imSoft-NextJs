420eeaa6203c62f0bc215999b5c520e9d0b9172c
imSoft <weareimsoft@gmail.com>
2023-02-27 23:49:26 -0600

    Prueba con Metaetiquetas

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import { Metatags } from "../components/metatags";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -18,6 +19,24 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/icons/favicon-imSoft.png" />
+          <Metatags
+            title={"imSoft"}
+            description={
+              "¿Necesitas contactarnos para resolver una duda, solicitar información o contratar nuestros servicios? ¡No dudes en hacerlo! En nuestro sitio web encontrarás un formulario de contacto diseñado específicamente para que puedas comunicarte con nosotros de manera sencilla y eficaz."
+            }
+            keywords={
+              "imSoft, pagina web, paginas web, sitio web, sitios web, tienda en linea, tienda electronica, ecommerce, seo, consultoria"
+            }
+            author={"Brandon Uriel García Ramos"}
+            subject={"Inicio"}
+            date={"01/01/2023"}
+            type={"Inicio"}
+            source={"https://www.imsoft.io/"}
+            image={
+              "https://firebasestorage.googleapis.com/v0/b/imsoft-website.appspot.com/o/Logos%20Empresa%2FLogotipo%20imSoft.png?alt=media&token=4b71448f-4047-402f-8b41-82a6c5e59ec4"
+            }
+            url={"https://www.imsoft.io/"}
+          />
         </Head>
         <body>
           <Main />
