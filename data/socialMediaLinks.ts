b0fee03473668214f2186184c852a3158330152d
imSoft <weareimsoft@gmail.com>
2023-03-12 14:39:42 -0600

    Se actualizo enlace de LinkedIn

--- data/socialMediaLinks.ts
+++ data/socialMediaLinks.ts
@@ -1,6 +1,6 @@
 const facebookLink = "https://www.facebook.com/weareimsoft";
 const instagramLink = "https://www.instagram.com/weareimsoft/";
-const linkedInLink = "https://www.linkedin.com/in/imsoft-%E2%80%8E-a3a236219/";
+const linkedInLink = "https://www.linkedin.com/company/imsoft/";
 const twitterLink = "https://twitter.com/weareimsoft";
 const tiktokLink = "https://www.tiktok.com/@weareimsoft";
 const whatsappBusinessLink = "https://api.whatsapp.com/message/6P7VAOCL442IH1?autoload=1&app_absent=0";
