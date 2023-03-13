dd08ceb4c4c718f8a55c7c2a1268906358ac6987
imSoft <weareimsoft@gmail.com>
2023-03-12 21:24:20 -0600

    Se agrego confetti a la pantalla de mensaje confirmado

--- pages/mensajes/mensajeDeConfirmacion.tsx
+++ pages/mensajes/mensajeDeConfirmacion.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 
 import MessageComponent from "../../components/ui/shared/MessageComponent";
 
 import { IMetatags, INotificationMessage } from "../../interfaces";
 import { dateMetatagInfo } from "../../data";
+import { showConfetti } from "../../utils";
 
 const MensajeDeConfirmacion = () => {
   const notificationMessageInfo: INotificationMessage = {
@@ -27,6 +28,10 @@ const MensajeDeConfirmacion = () => {
     url: "https://www.imsoft.io/mensajes/mensajeDeConfirmacion",
     robots: "index,follow",
   };
+
+  useEffect(() => {
+    showConfetti();
+  }, []);
 
   return (
     <>
